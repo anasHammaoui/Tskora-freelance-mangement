21c1c9cbfb9657eb8e0a123e0c4abb7054aa2ee4
anasHammaoui <anashammaoui07@gmail.com>
2026-01-17 09:00:00 +0100

    add ResourceNotFoundException

---
+++ src/main/java/com/example/Taskora/exception/ResourceNotFoundException.java
@@ -0,0 +1,11 @@
+package com.example.Taskora.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+    public ResourceNotFoundException(String resource, Long id) {
+        super(resource + " not found with id: " + id);
+    }
+}
